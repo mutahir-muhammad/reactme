a9063f12f91a94171408da30f4cd9bd602193829
Mutahir <mutahir.khan44@yahoo.com>
2025-02-18 00:33:06 +0500

    built basic site structure

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ Correct import for React 18
+import App from "./App";
+import "./main.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
